cfa508769895abd1d479b821132c95f8f20dfbde
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-11-30 15:23:04 -0300

    Terminando de configurar

--- cypress/e2e/ibge/busca.js
+++ cypress/e2e/ibge/busca.js
@@ -1,17 +1,17 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given("que estou na página inicial do IBGE", () => {
-  cy.visit("https://www.ibge.gov.br/");
+  cy.visit("/");
 });
 
 When('eu digito "população" no campo de busca', () => {
-  cy.get("#pesquisa-portal-input").type("população", { delay: 80 });
+  cy.get('input[type="search"]').type("população", { delay: 80 });
 });
 
 When("eu clico no botão de pesquisa", () => {
-  cy.get("#pesquisa-portal-submit").click();
+  cy.get('button[class*="search"]').click();
 });
 
 Then("devo ver resultados relacionados à população", () => {
-  cy.contains(/população/).should("exist");
+  cy.contains(/população/i).should("exist");
 });
