c7f0dbb1e985219680ecefcb14fc226f9d2cd717
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-11-30 10:55:17 -0300

    Terminando de configurar

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,27 @@
 const { defineConfig } = require("cypress");
-const cucumber = require("cypress-cucumber-preprocessor").default;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const bundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const cucumber =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+
 module.exports = defineConfig({
   e2e: {
     specPattern: "**/*.feature",
-    setupNodeEvents(on, config) {
-      on("file:preprocessor", cucumber());
-    },
-  },
+    baseUrl: "https://forager.fandom.com/wiki",
+
+    async setupNodeEvents(on, config) {
+      // Plugin moderno
+      await cucumber(on, config);
+
+      on(
+        "file:preprocessor",
+        bundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
+    }
+  }
 });
-    
